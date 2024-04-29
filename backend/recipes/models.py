ab2e48771b2785a55138e5503faf63879437b8be
gabriel moreno <gabriel2043579@gmail.com>
2024-04-28 20:38:49 -0400

    creation of the models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Recipes(models.Model):
+ name = models.CharField(max_length=200)
+ description = models.TextField(max_length=250)
+class Ingredients(models.Model):
+ name = models.CharField(max_length=200)
+
+class Ingredient_recipes(models.Model):
+ recipe_id = models.ForeignKey(Recipes, on_delete=models.CASCADE)
+ Ingredient_id = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+ Ingredient_quantity = models.IntegerField()
+    
