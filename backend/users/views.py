120ac72fdf797ff73454130ecb08931254b29d6b
JDRL555 <joshuleal3@gmail.com>
2024-05-14 14:08:52 -0400

    implemented the user not found validation

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,28 +1,30 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-from django.shortcuts import get_list_or_404
 from rest_framework.decorators import authentication_classes,permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
 from django.contrib.auth.models import User
-# Create your views here.
 
+from .serializers import UserSerializer
 
 @api_view(['POST'])
 def login(request):
+  global user
   
-  user = User.objects.get(username=request.data['username'])
+  try:
+    user = User.objects.get(username=request.data['username'])
+  except:
+    return Response({"error": "Invalid username"}, status=status.HTTP_400_BAD_REQUEST)
   
   if not user.check_password(request.data['password']):
     return Response({"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
   
-  token,created = Token.objects.get_or_create(user=user)
+  token, created = Token.objects.get_or_create(user=user)
   serializer = UserSerializer(instance=user)
-   
+
   return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
